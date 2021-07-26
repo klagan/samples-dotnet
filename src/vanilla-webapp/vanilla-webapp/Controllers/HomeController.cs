510b65dcef3f2c18fd126dfaa2bf31a8720fe805
Kam Lagan <k.lagan@gmail.com>
2021-07-26 10:01:45 +0100

    - add vanilla -webapp

---
+++ src/vanilla-webapp/vanilla-webapp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using vanilla_webapp.Models;
+
+namespace vanilla_webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var environmentVariable = Environment.GetEnvironmentVariable("kamTestVariable") ?? "[NONE FOUND]";
+            var machineName = System.Environment.MachineName;
+            var isContainer = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+
+            ViewBag.EnvironmentVar = environmentVariable;
+            ViewBag.MachineName = machineName;
+            ViewBag.IsContainer = isContainer;
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
